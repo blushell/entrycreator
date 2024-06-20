11380641fa915bdffb49d8f50557c2e35687638a
blushell <micheal.jones89@gmail.com>
2024-06-19 23:57:05 -0400

    base setup

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer">
+			<nav>
+				<ul>
+					<li><NuxtLink to="/index">Home</NuxtLink></li>
+					<li><NuxtLink to="/devkits">Dev Kits</NuxtLink></li>
+				</ul>
+			</nav>
+		</v-navigation-drawer>
+
+		<v-app-bar>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+			<v-app-bar-title>Application</v-app-bar-title>
+		</v-app-bar>
+
+		<v-main>
+			<slot></slot>
+		</v-main>
+	</v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const drawer = ref(null);
+</script>
+
+<script>
+export default {
+	data: () => ({ drawer: null }),
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<v-card>
+		<v-card-title>
+			<v-icon>mdi-power-plug</v-icon> Entry.tp Builder
+		</v-card-title>
+		<v-card-text>
+			<plugin-setup @updateData="updateData" />
+			<plugin-data :formData="formData" />
+		</v-card-text>
+	</v-card>
+</template>
+<script>
+export default {};
+</script>
+<style>
+body {
+	padding: 2em;
+}
+.component {
+	padding: 25px 0 25px 0;
+}
+</style>
